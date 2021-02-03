82fb38d610c150fa4ff3491c20eb50e8b0ace7e4
Patrik Ramos <45246753+patrikramos@users.noreply.github.com>
2021-02-03 19:51:03 -0300

    Add files via upload

---
+++ assets/script/works_app.js
@@ -0,0 +1,16 @@
+
+(function() {
+	'use strict';
+
+// TODO init service worker
+// 	if ('serviceWorker' in navigator) {
+// 		navigator.serviceWorker
+// 			.register('serviceWorker/rsbuilder-sw.js')
+// 			.then(function() { console.log('Service Worker Registered'); });
+//
+// 		navigator.serviceWorker
+// 			.register('serviceWorker/rsbuilder-sw-register.js')
+// 			.then(function() { console.log('Service Worker Registered'); });
+// 	}
+
+})();
